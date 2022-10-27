397adb9f576f590319b534dd0aeed89bb3539c95
gmendezg <gmendezg@gmail.com>
2022-10-27 03:48:40 +0200

    Update run.sh

--- acme_sh/run.sh
+++ acme_sh/run.sh
@@ -16,10 +16,10 @@ for domain in $DOMAINS; do
     DOMAIN_ARR+=(--domain "$domain")
 done
 
-/root/.acme.sh/acme.sh --register-account -m ${ACCOUNT}
+#/root/.acme.sh/acme.sh --register-account -m ${ACCOUNT}
 
-/root/.acme.sh/acme.sh --issue "${DOMAIN_ARR[@]}" \
---dns "$DNS_PROVIDER"
+/root/.acme.sh/acme.sh --insecure --issue "${DOMAIN_ARR[@]}" \
+--dns "$DNS_PROVIDER" --yes-I-know-dns-manual-mode-enough-go-ahead-please 
 
 /root/.acme.sh/acme.sh --install-cert "${DOMAIN_ARR[@]}" \
 --fullchain-file "/ssl/${CERTFILE}" \
